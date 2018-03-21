a751ca07b8b87302661bd1c0ebdd4bb5360ec030
Melanie Cooke <melanie.angela.cooke@gmail.com>
2018-03-21 16:32:06 -0400

    Initial commit. Followed tutorial for a basic firebase and vue.js setup from Medium.com by Anas mammeri.

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login">
+        <h3>Log In</h3>
+        <input type="text" v-model="email" placeholder="Email"><br>
+        <input type="password" v-model="password" placeholder="Password"><br>
+        <button v-on:click="login">Log in</button>
+        <p>Don't have an account? You can <router-link to="/signup">create one.</router-link></p>
+    </div>
+</template>
+
+<script>
+    import firebase from 'firebase'
+
+    export default {
+        name: 'login',
+        data: function() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login: function() {
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+                    (user) => {
+                        this.$router.replace('hello')
+                    },
+                    (err) => {
+                        alert('Oops, ' + err.message)
+                    }
+                );
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 20px;
+        cursor: pointer;
+    }
+    p {
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+let app;
+// Initialize Firebase
+let config = {
+  apiKey: "AIzaSyAG-gMvfEY1zz598izs4b3vf1a8HoXhB70",
+  authDomain: "monstore-26d9d.firebaseapp.com",
+  databaseURL: "https://monstore-26d9d.firebaseio.com",
+  projectId: "monstore-26d9d",
+  storageBucket: "monstore-26d9d.appspot.com",
+  messagingSenderId: "786970198271"
+};
+
+firebase.initializeApp(config)
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      router,
+      components: { App },
+      template: '<App/>'
+    })
+  }
+});
+
+
+

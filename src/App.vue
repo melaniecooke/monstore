ab985e6a0905f3a8a6d12f91c7906b93f622ff1a
Albert VanderMeulen <albertjvm@users.noreply.github.com>
2018-03-25 22:35:18 -0400

    header and slideout hamburger menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div v-on:click="toggleMenu" class="hamburger">
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+    <div v-bind:class="{menu: true, active: isActive}">
+      <div class="item" v-on:click="goto('hello')">Home</div>
+      <div class="item" v-on:click="goto('monsters')">Monsters</div>
+      <div class="item" v-on:click="goto('lists')">My Lists</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isActive = !this.isActive
+    },
+    goto (route) {
+      this.$router.replace(route)
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .hamburger {
+    cursor: pointer;
+    height: 25px;
+    width: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .bar {
+    background-color: white;
+    height: 5px;
+    width: 100%;
+  }
+  .menu {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    top: 60px;
+    right: 0;
+    height: calc(100vh - 60px);
+    width: 0;
+    transition: width 0.5s;
+    background-color: #42b983;
+  }
+  .menu.active {
+    width: 200px;
+  }
+  .item {
+    user-select: none;
+    cursor: pointer;
+    line-height: 50px;
+    color: white;
+    font-size: 24px;
+    font-weight: bolder;
+    width: 100%;
+    text-align: center;
+    white-space: nowrap;
+  }
+  .item:hover {
+    background-color: lighten(#42b983, 10%);
+  }
+</style>
